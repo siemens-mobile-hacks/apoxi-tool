1a0ba575d12d28346b367536a76dd2ddc5ae9323
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2025-08-10 21:57:25 +0000

    fixes

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { program } from "commander";
 import { cliListMemory, cliReadAllMemory, cliReadMemory } from "./cli/memory.js";
 import { createAppCommand } from "./utils/command.js";
